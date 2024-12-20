d8ea37a026ba88276b19431c7c717f882010c5fe
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-12-20 20:08:56 +0000

    Compiler complains about postcss for some reason

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -4,9 +4,9 @@
     const user = $page.data.user;
 </script>
 
-<a href="/about">About Us</a>
-<a href="/events">Events</a>
-<a href="/sponsors">Sponsors</a>
+<a class="btn btn-sm hover:variant-ringed-primary" href="/about">About Us</a>
+<a class="btn btn-sm hover:variant-ringed-primary" href="/events">Events</a>
+<a class="btn btn-sm hover:variant-ringed-primary" href="/sponsors">Sponsors</a>
 
 <!-- {#if user}
     <form action="/auth/signout" method="POST" class="text-center">
@@ -17,9 +17,3 @@
         <button class="btn btn-sm variant-outline-primary">Sign in</button>
     </form>
 {/if} -->
-
-<style lang="postcss">
-    a {
-        @apply btn btn-sm hover:variant-ringed-primary;
-    }
-</style>
