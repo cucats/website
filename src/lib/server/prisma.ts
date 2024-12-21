af62336834a03e056797cd3bfc4cb4c2983730c6
IntGrah <148660026+IntGrah@users.noreply.github.com.>
2024-12-21 13:02:13 +0000

    Format

--- src/lib/server/prisma.ts
+++ src/lib/server/prisma.ts
@@ -1,9 +1,9 @@
-import { PrismaClient as ImportedPrismaClient } from '@prisma/client';
-import { createRequire } from 'module';
+import { PrismaClient as ImportedPrismaClient } from "@prisma/client";
+import { createRequire } from "module";
 
 const require = createRequire(import.meta.url);
 
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 
 const prisma: ImportedPrismaClient = new PrismaClient();
 
