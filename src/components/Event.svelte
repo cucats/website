dce41fd151a19070e1837b21498800a93b982818
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-01-27 20:57:17 +0000

    Use component for events

---
+++ src/components/Event.svelte
@@ -0,0 +1,50 @@
+<script>
+    export let event = {};
+</script>
+
+<div class="event-card">
+    <div class="title">{event.title}</div>
+
+    {#if event.date}
+        <div class="date">{event.date[0]} &ndash; {event.date[1]}</div>
+    {/if}
+
+    {#if event.description}
+        <p>{event.description}</p>
+    {/if}
+
+    {#if event.website}
+        <a href={event.website} class="btn variant-soft-primary">Website</a>
+    {/if}
+
+    {#if event.logo}
+        <img src={event.logo} alt="{event.title} logo" />
+    {/if}
+</div>
+
+<style lang="postcss">
+    .event-card {
+        @apply bg-opacity-60 bg-zinc-900 whitespace-pre-line p-8 md:p-10 md:mx-8 rounded-lg relative text-center md:text-justify;
+
+        .title {
+            @apply text-4xl font-bold;
+        }
+
+        .date {
+            @apply uppercase;
+        }
+
+        p {
+            @apply mt-4 text-gray-100 whitespace-normal;
+        }
+
+        a {
+            @apply mt-4;
+        }
+
+        img {
+            @apply max-h-full flex absolute right-0 top-0 opacity-10 -z-10;
+        }
+    }
+
+</style>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { fade } from "svelte/transition";
+    import Event from "../../components/Event.svelte";
     import events from "../../data/events.json";
     let now = new Date().getTime();
 </script>
@@ -9,25 +10,7 @@
 
     {#each events as event}
         {#if !event.date || Date.parse(event.date[1]) > now}
-            <div class="event-card">
-                <div class="title">{event.title}</div>
-
-                {#if event.date}
-                    <div class="date">{event.date[0]} &ndash; {event.date[1]}</div>
-                {/if}
-
-                {#if event.description}
-                    <p>{event.description}</p>
-                {/if}
-
-                {#if event.website}
-                    <a href={event.website} class="btn variant-soft-primary">Website</a>
-                {/if}
-
-                {#if event.logo}
-                    <img src={event.logo} alt="{event.title} logo" />
-                {/if}
-            </div>
+            <Event event={event} />
         {/if}
     {/each}
 
@@ -37,51 +20,8 @@
 
     {#each events as event}
         {#if event.date && Date.parse(event.date[1]) <= now}
-            <div class="event-card">
-                <div class="title">{event.title}</div>
-
-                {#if event.date}
-                    <div class="date">{event.date[0]} &ndash; {event.date[1]}</div>
-                {/if}
-
-                {#if event.description}
-                    <p>{event.description}</p>
-                {/if}
-
-                {#if event.website}
-                    <a href={event.website} class="btn variant-soft-primary">Website</a>
-                {/if}
-
-                {#if event.logo}
-                    <img src={event.logo} alt="{event.title} logo" />
-                {/if}
-            </div>
+            <Event event={event} />
         {/if}
     {/each}
 </main>
 
-<style lang="postcss">
-    .event-card {
-        @apply bg-opacity-60 bg-zinc-900 whitespace-pre-line p-8 md:p-10 md:mx-8 rounded-lg relative text-center md:text-justify;
-    }
-
-    .title {
-        @apply text-4xl font-bold;
-    }
-
-    .date {
-        @apply uppercase;
-    }
-
-    .event-card p {
-        @apply mt-4 text-gray-100 whitespace-normal;
-    }
-
-    .event-card a {
-        @apply mt-4;
-    }
-
-    .event-card img {
-        @apply max-h-full flex absolute right-0 top-0 opacity-10 -z-10;
-    }
-</style>
