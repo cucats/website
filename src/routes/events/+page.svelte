ae63fb3ffd3d5d4c13ada7ebe48ba8e74378f437
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-01-27 21:03:49 +0000

    Display events on end day

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -2,14 +2,16 @@
     import { fade } from "svelte/transition";
     import Event from "../../components/Event.svelte";
     import events from "../../data/events.json";
-    let now = new Date().getTime();
+
+    // Should still display on end date
+    let today = new Date().setHours(0, 0, 0, 0);
 </script>
 
 <main class="p-4 flex flex-col gap-y-4 max-w-screen-lg mx-auto lg:pt-12" in:fade>
     <h1 class="text-5xl">Events</h1>
 
     {#each events as event}
-        {#if !event.date || Date.parse(event.date[1]) > now}
+        {#if !event.date || Date.parse(event.date[1]) >= today}
             <Event event={event} />
         {/if}
     {/each}
@@ -19,7 +21,7 @@
     <h2 class="my-4 text-4xl">Archive</h2>
 
     {#each events as event}
-        {#if event.date && Date.parse(event.date[1]) <= now}
+        {#if event.date && Date.parse(event.date[1]) < today}
             <Event event={event} />
         {/if}
     {/each}
