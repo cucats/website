cf4f3d40dccd4241531047d0f44724ee585feec3
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-01-28 02:17:43 +0000

    Load sponsors from json

---
+++ src/lib/components/Sponsor.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let sponsor = {};
+</script>
+
+<section class="sponsor-card">
+    <img src={sponsor.logo} alt="{sponsor.name} logo" />
+
+    {#each sponsor.paragraphs as paragraph}
+        <p>{paragraph}</p>
+    {/each}
+
+    <div class="button-container">
+        {#each Object.entries(sponsor.links) as [key, value]}
+            <a href={value} class="btn variant-outline-primary capitalize">{key}</a>
+        {/each}
+    </div>
+</section>
+
+<style lang="postcss">
+    .sponsor-card {
+        @apply flex flex-col w-full items-center gap-4 rounded-xl outline-primary-400 outline outline-1 bg-white bg-opacity-20 p-4 text-justify;
+
+        img {
+            @apply object-contain h-24 m-2;
+        }
+    }
+
+    .button-container {
+        @apply flex justify-center w-full pt-4 gap-x-8 gap-y-4 flex-wrap;
+    }
+</style>
--- src/routes/sponsors/+page.svelte
+++ src/routes/sponsors/+page.svelte
@@ -1,290 +1,33 @@
 <script lang="ts">
     import { fade } from "svelte/transition";
+    import Sponsor from "$lib/components/Sponsor.svelte";
+    import sponsors from "../../data/sponsors.json";
 </script>
 
 <main class="p-4 flex flex-col gap-y-4 max-w-screen-lg mx-auto lg:pt-12 lg:gap-y-8" in:fade>
     <h1 class="h1 mb-4">Sponsors</h1>
+
     <h2 class="h2">Gold</h2>
 
-    <section class="sponsor-card">
-        <img src="/sponsors/hrt.png" alt="Hudson River Trading"/>
-        <p>
-            At Hudson River Trading (HRT) we are mathematicians, computer scientists, statisticians,
-            physicists and engineers. We research and develop automated trading algorithms using
-            advanced mathematical techniques. We have built one of the world's most sophisticated
-            compute environments, and our researchers are at the forefront of innovation in the
-            world of algorithmic trading and machine learning.
-        </p>
-        <p>
-            HRT was founded in 2002 by computer scientists and mathematicians who believed that math
-            and technology could transform financial markets. They developed the algorithms that
-            became the foundation for HRT's continued success across global asset classes. Today, we
-            are global industry leaders, and a leading voice for fair and open markets. Our trading
-            creates stronger markets for all investors, and all our work is built upon our rigorous
-            code of ethics.
-        </p>
-        <p>
-            We're a community of self-starters, critical thinkers, and eager problem-solvers.
-            Collaboration is at the heart of our success — from an open-source code base to lively
-            cross-team lunches in our cafe, a spirit of openness underscores everything we do. We
-            celebrate great ideas from HRT veterans and new hires alike. We love solving the most
-            complex problems in trading and technology, and believe the best work can only be done
-            together.
-        </p>
-        <div class="button-container">
-            <a
-                href="https://www.hudsonrivertrading.com/"
-                class="btn variant-outline-primary"
-                target="_blank"
-            >
-                Website
-            </a>
-            <a
-                href="https://www.hudsonrivertrading.com/hrtbeat"
-                class="btn variant-outline-primary"
-            >
-                Blog
-            </a>
-        </div>
-    </section>
-
-    <section class="sponsor-card">
-        <img
-            src="/sponsors/js_stacked.png"
-            alt="Jane Street Logo"
-            class="invert"
-        />
-        <p>
-            Our INSIGHT program is part of our commitment to give as many people as possible the
-            chance to learn more about what we do.
-        </p>
-        <p>
-            In this multi-day program, we invite women, transgender, and/or gender-expansive
-            individuals to learn about the roles within our firm and how we solve challenges at Jane
-            Street — including the specific concepts and tools we use in our daily work.
-        </p>
-        <p>
-            As you explore opportunities within the program, you'll notice that event topics and
-            locations will vary. We offer classes and activities in three separate tracks: software
-            engineering, trading, and strategy and product.
-        </p>
-        <p>
-            Our next INSIGHT event based in our London office will run Monday, 7th April - Saturday,
-            12th April, 2025. Candidates will be notified about their applications status after the
-            deadline date. The deadline to apply is Sunday, 12th January 2025 at 11:59pm GMT.
-        </p>
-        <div class="button-container">
-            <a
-                href="https://janestreet.com/apply-insight-ldn/?utm_source=program_email&utm_medium=email&utm_campaign=insight_ldn_program&utm_content=apply_button"
-                class="btn variant-outline-primary"
-            >
-                Apply here
-            </a>
-            <a
-                href="https://www.janestreet.com/join-jane-street/open-roles/?type=internship&location=london"
-                class="btn variant-filled-primary"
-            >
-                View Opportunities
-            </a>
-        </div>
-    </section>
-
-    <section class="sponsor-card">
-        <img src="/sponsors/qrt.svg" alt="QRT Logo"  />
-        <p>
-            Qube Research & Technologies (QRT) is a global investment manager where we deploy a
-            diverse range of investment strategies across geographies, asset classes and time
-            frames. Combining data, research, technology and trading expertise has shaped QRT's
-            collaborative mindset which enables us to solve the most complex challenges. QRT's
-            culture of innovation continuously drives our ambition to deliver high quality returns
-            for our investors.
-        </p>
-        <div class="button-container">
-            <a href="https://www.qube-rt.com/" class="btn variant-outline-primary">Website</a>
-            <a href="https://www.qube-rt.com/careers/" class="btn variant-filled-primary">
-                View Opportunities
-            </a>
-        </div>
-    </section>
+    {#each sponsors as sponsor}
+        {#if sponsor.tier == "gold"}
+            <Sponsor {sponsor} />
+        {/if}
+    {/each}
 
     <h2 class="h2">Silver</h2>
 
-    <section class="sponsor-card">
-        <div class="flex items-center">
-            <img src="/sponsors/nustom.svg" alt="Nustom logo" />
-            <span class="select-none text-4xl">Nustom</span>
-        </div>
-        <p>
-            We're a startup that makes startups! We use AI to minimise the cost of building,
-            operating and growing startups. Previously, we helped build Monzo. We're founded in
-            March 2024 by former Monzo Co-Founder and CTO and backed by some of the world's best
-            investors (Garry Tan, Naval Ravikant, Tom Blomfield, Accel, Index Ventures, OpenAI,
-            founders of Anthropic, Perplexity, etc).
-        </p>
-        <p>
-            In a few years' time, we believe that the majority of software will be made by people
-            with no technical background, collaborating with an AI. What today takes weeks or months
-            and a team of specialists will be accessible to everyone, take only minutes, and cost
-            very little. Instead of one-size-fits-all products, everyone will be making their own
-            tools, customized to their needs.
-        </p>
-        <p>
-            Come spend 3 or 6 months with us to build and launch a revenue generating startup! You
-            can build whatever you like. The only catch is that you have to use our AI generator to
-            build your startup from human language input. And if it's not able to do so, you first
-            need to improve the generator to make it generatable.
-        </p>
-        <div class="button-container">
-            <a href="https://nustom.com/" class="btn variant-outline-primary">Website</a>
-            <a
-                href="https://nustom.notion.site/150622a338eb802aa204efb2997f7202"
-                class="btn variant-filled-primary"
-            >
-                Apply Now
-            </a>
-        </div>
-    </section>
-
-    <section class="sponsor-card">
-        <img src="/sponsors/optiver.png" alt="Optiver Logo" />
-        <p>
-            Unlock the world of algorithmic trading with The Optiver & Cambridge Trading Academy.
-            Led by industry experts, this comprehensive 8-session course takes you from options
-            theory to hands-on Python algorithm design.
-        </p>
-        <p>
-            Test your skills on Optiver's 24/7 simulated market, Optibook, and compete in pairs to
-            win an exciting prize. Gain practical skills and knowledge directly applicable to a
-            career in finance and trading. <a
-                href="https://optiver.com/recruitment-events/save-the-date-the-optiver-cambridge-trading-academy/"
-            >
-                Register
-            </a>
-            to express your interest in joining the academy and be the first to know when applications
-            open!
-        </p>
-        <div class="button-container">
-            <a
-                href="https://optiver.com/recruitment-events/save-the-date-the-optiver-cambridge-trading-academy/"
-                class="btn variant-outline-primary"
-            >
-                Register Interest
-            </a>
-            <a
-                href="https://optiver.com/working-at-optiver/career-opportunities/page/2/?search=internship"
-                class="btn variant-filled-primary"
-            >
-                View Opportunities
-            </a>
-        </div>
-    </section>
-
-    <section class="sponsor-card">
-        <img
-            src="/sponsors/quantco.png"
-            alt="Quantco Logo"
-            class="invert object-contain h-24 m-2"
-        />
-        <p>
-            We leverage expertise in data science, engineering, artificial intelligence, and
-            economics to help organizations turn data into decisions. Founded by 4 PhDs from Harvard
-            and Stanford, we are now more than 200 professionals with extensive quantitative,
-            engineering, and business acumen.
-        </p>
-        <p>
-            Our teams apply cutting-edge research to solve real-world problems within algorithmic
-            pricing, fraud detection, and cancer diagnostics. Our customers include some of the
-            largest financial, retail, and healthcare organizations in the US and Europe.
-        </p>
-        <p>
-            We're a rapidly scaling company that offers full-time and internship opportunities in
-            London, Zurich, Berlin, Munich, Karlsruhe, Cologne, Boston and San Francisco.
-        </p>
-        <div class="button-container">
-            <a href="https://www.quantco.com/" class="btn variant-outline-primary">Website</a>
-            <a href="https://jobs.lever.co/quantco-" class="btn variant-filled-primary">
-                View Opportunities
-            </a>
-        </div>
-    </section>
+    {#each sponsors as sponsor}
+        {#if sponsor.tier == "silver"}
+            <Sponsor {sponsor} />
+        {/if}
+    {/each}
 
     <h2 class="h2">Bronze</h2>
 
-    <section class="sponsor-card">
-        <img src="/sponsors/imc.png" alt="IMC Trading" />
-        <p>
-            IMC is a leading trading firm, known worldwide for our advanced, low-latency technology
-            and world-class execution capabilities. Over the past 35 years, we've been a stabilizing
-            force in the financial markets &ndash; providing the essential liquidity our
-            counterparties depend on. Across offices in the US, Europe, and Asia Pacific, our
-            talented employees are united by our entrepreneurial spirit, exceptional culture, and
-            commitment to giving back. It's a strong foundation that allows us to grow and add new
-            capabilities, year after year. From entering dynamic new markets, to developing a
-            state-of-the-art research environment and diversifying our trading strategies, we dare
-            to imagine what could be and work together to make it happen.
-        </p>
-        <div class="button-container">
-            <a href="https://grnh.se/863b7422teu" class="btn variant-outline-primary">
-                View Opportunities
-            </a>
-        </div>
-    </section>
-
-    <section class="sponsor-card">
-        <img src="/sponsors/TPP.png" alt="TPP Logo" />
-        <p>
-            TPP is a global digital health company. With over 7,000 organisations using our
-            solutions to care for over 50 million patients, our software is used across all health
-            and social care settings, including GPs, emergency departments, hospitals and mental
-            health services. This means that wherever and whenever a patient needs care, a detailed
-            and up-to-date record is available. No other company has a digital healthcare solution
-            on this scale. Our database is one of the largest in the world. It processes a billion
-            transactions daily – more than the London Stock Exchange and Visa combined.
-        </p>
-        <p>
-            Our technology helps improve people's lives across the world, whether it is scheduling
-            immunisations for millions of children, allowing doctors to manage complex care for
-            elderly patients, helping governments with the prevention of outbreaks, or developing
-            new machine learning algorithms for the early diagnosis of disease.
-        </p>
-        <p>
-            TPP is committed to helping tackle global health challenges, working with governments to
-            deliver the technology required for the future of healthcare.
-        </p>
-        <p>
-            We are always looking for talented and motivated students to join our team. If you are
-            interested in functional programming, compilers, programming languages, or distributed
-            systems, we would love to hear from you. We offer internships, summer projects, and PhD
-            positions.
-        </p>
-        <div class="button-container">
-            <a href="https://tpp-careers.com/" class="btn variant-outline-primary"
-                >View Opportunities</a
-            >
-            <a
-                href="https://tpp-careers.com/roles/graduate-software-developer/"
-                class="btn variant-filled-primary"
-            >
-                Graduate Roles
-            </a>
-        </div>
-    </section>
+    {#each sponsors as sponsor}
+        {#if sponsor.tier == "bronze"}
+            <Sponsor {sponsor} />
+        {/if}
+    {/each}
 </main>
-
-<style lang="postcss">
-    .sponsor-card {
-        @apply flex flex-col w-full items-center gap-4 rounded-xl outline-primary-400 outline outline-1 bg-white bg-opacity-20 p-4;
-
-        & a:not(.btn) {
-            @apply text-primary-400 underline;
-        }
-
-        img {
-            @apply object-contain h-24 m-2;
-        }
-    }
-
-    .button-container {
-        @apply flex justify-center w-full pt-4 gap-x-8 gap-y-4 flex-wrap;
-    }
-</style>
