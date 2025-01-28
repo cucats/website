e44455264b4f2288ac3d854b1af4f6bbe1409b33
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-01-28 14:53:28 +0000

    Move to sponsor cards

--- src/lib/components/Sponsor.svelte
+++ src/lib/components/Sponsor.svelte
@@ -1,10 +1,18 @@
 <script>
     export let sponsor;
+
+    let colours = new Map([
+        ["gold", ["#ffc000", "#ffa000"]],
+        ["silver", ["#cccccc", "#888888"]],
+        ["bronze", ["#ffa000", "#cc8033"]],
+    ])
 </script>
 
-<section class="sponsor-card">
+<!-- Couldn't get tailwind to work so just set style manually... -->
+<div class="sponsor-card" style="background: linear-gradient({colours.get(sponsor.tier)?.[0]}, {colours.get(sponsor.tier)?.[1]})">
     <img src={sponsor.logo} alt="{sponsor.name} logo" />
 
+    <!--
     {#each sponsor.paragraphs as paragraph}
         <p>{paragraph}</p>
     {/each}
@@ -14,14 +22,15 @@
             <a href={value} class="btn variant-outline-primary capitalize">{key}</a>
         {/each}
     </div>
-</section>
+    -->
+</div>
 
 <style lang="postcss">
     .sponsor-card {
-        @apply flex flex-col w-full items-center gap-4 rounded-xl outline-primary-400 outline outline-1 bg-white bg-opacity-20 p-4 text-justify;
+        @apply flex flex-col w-96 items-center rounded-xl p-4 shadow-sm hover:shadow-2xl hover:scale-105 transition mx-auto;
 
         img {
-            @apply object-contain h-24 m-2;
+            @apply object-contain h-24 my-8;
         }
     }
 
