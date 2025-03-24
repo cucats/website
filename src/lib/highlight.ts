49d0c93e0811528a4855ecbaa873c9c60c694246
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-03-24 16:32:49 +0000

    Remove hightlight.js

--- src/lib/highlight.ts
+++
@@ -1,9 +0,0 @@
-import { storeHighlightJs } from "@skeletonlabs/skeleton";
-import "highlight.js/styles/github-dark.css";
-import hljs from "highlight.js/lib/core";
-import python from "highlight.js/lib/languages/python";
-
-hljs.registerLanguage("lean", python);
-storeHighlightJs.set(hljs);
-
-export default hljs;
