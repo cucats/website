6c630e30636d5d24ecc227c69d6703f0208d2b5b
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-03-25 01:50:54 +0000

    Remove outer box for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,17 +8,15 @@
     in:fade
 >
     <Grid />
-    <div class="card variant-soft-secondary rounded-3xl p-5">
-        <div class="flex flex-col items-center lg:flex-row lg:gap-8">
-            <img src="/logo/dark/logo-white-cat.svg" alt="Logo" class="size-48" />
-            <h1 class="h2 text-center lg:h1">
-                Cambridge Uni Computing
-                <br />
-                and Technology Society
-            </h1>
-        </div>
-        <p class="mx-10 text-center">
-            The main student-led CS and tech society at the University of Cambridge!
-        </p>
+    <div class="flex flex-col items-center lg:flex-row lg:gap-8">
+        <img src="/logo/dark/logo-white-cat.svg" alt="Logo" class="size-48" />
+        <h1 class="h2 text-center lg:h1">
+            Cambridge Uni Computing
+            <br />
+            and Technology Society
+        </h1>
     </div>
+    <p class="mx-10 text-center">
+        The main student-led CS and tech society at the University of Cambridge!
+    </p>
 </main>
