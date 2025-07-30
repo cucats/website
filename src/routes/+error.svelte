a7a56431c95c0d5399fa832eb325a448c6ea3c36
IntGrah <148660026+IntGrah@users.noreply.github.com>
2025-07-30 20:12:27 +0100

    Sensible 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,9 +4,11 @@
   const { error, status } = page;
 </script>
 
-<section class="bg-secondary-500 pt-24">
-  <div class="c-4 mx-auto max-w-5xl p-4">
-    <h1 class="text-4xl font-bold md:text-5xl">{status}</h1>
-    <h2 class="text-3xl font-bold md:text-4xl">{error?.message}</h2>
-  </div>
-</section>
+<main class="bg-primary-800">
+  <section class="pt-24 text-neutral-100">
+    <div class="c-4 mx-auto max-w-4xl p-4">
+      <h1 class="text-4xl font-bold md:text-5xl">{status}</h1>
+      <h2 class="text-3xl font-bold md:text-4xl">{error?.message}</h2>
+    </div>
+  </section>
+</main>
